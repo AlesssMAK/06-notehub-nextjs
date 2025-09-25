efcf3df7252163e0a61e46d4651ced82f72d5075
Alexey <makovii88@gmail.com>
2025-09-25 17:35:06 +0200

    update componets

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { Note, NoteTag } from '../types/note';
 
-const myKey = process.env.VITE_NOTEHUB_TOKEN;
+const myKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
 axios.defaults.baseURL = 'https://notehub-public.goit.study/api/notes';
 axios.defaults.headers.common.Authorization = `Bearer ${myKey}`;
 
