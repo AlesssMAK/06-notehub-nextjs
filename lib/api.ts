d695a405bb63838fdbe3989ccd6b23d2e2de885b
Oleksii Makovii <makovii88@gmail.com>
2025-09-29 08:00:34 +0200

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -19,7 +19,7 @@ interface NewNoteContent {
 export const fetchNotes = async (
   search: string,
   page: number,
-  perPage = 16
+  perPage = 15
 ): Promise<NoteHttpRequest> => {
   const params = {
     search,
